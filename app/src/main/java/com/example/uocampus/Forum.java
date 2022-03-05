3433ce5abd9ec3db694f862d8b0aee3a8ea042f2
Woodyhu96 <woodyhu@udel.edu>
2022-03-05 14:07:28 +0800

    Add forum page and button function to MainAct

---
+++ app/src/main/java/com/example/uocampus/Forum.java
@@ -0,0 +1,17 @@
+package com.example.uocampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+
+//        forum detail here
+
+    }
+}
--- app/src/main/java/com/example/uocampus/MainActivity.java
+++ app/src/main/java/com/example/uocampus/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.uocampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Forum_botton,Appoint_botton,Map_botton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Forum_botton = (Button) findViewById(R.id.forum);
+        Appoint_botton = (Button) findViewById(R.id.appointment);
+        Map_botton = (Button) findViewById(R.id.map);
+
+        Forum_botton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Forum.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
