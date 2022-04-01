c02279dc57daf8a68e8bce76a4231adfafd40d25
Woodyhu96 <woodyhu@udel.edu>
2022-04-02 00:50:05 +0800

    add fb and twtr implem.

--- app/src/main/java/com/example/uocampus/forum/Forum_Page.java
+++ app/src/main/java/com/example/uocampus/forum/Forum_Page.java
@@ -5,16 +5,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import android.app.ListActivity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.net.Uri;
+
+import com.example.uocampus.MainActivity;
 import com.example.uocampus.R;
 
 public class Forum_Page extends ListActivity {
+
+    private static final String TAG = Forum_Page.class.getSimpleName();
     public DBControl loader;
     public List<Submit_Post_Func> list = new ArrayList<>();
     public List listitem = new ArrayList();
@@ -53,11 +65,80 @@ public class Forum_Page extends ListActivity {
         ListView listView = (ListView) this.findViewById(android.R.id.list);
         listView.setAdapter(adpt);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Map<String, Object> map = (Map<String, Object>) parent.getItemAtPosition(position);
+                Object sharable = map.get("content");
+                Log.i(TAG,"Item clicked "+ sharable);
+                String sr = (String)sharable;
+//                share(sr);
+//                onShareClick(sr);
             }
+
         });
 
     }
+    public void share(String sr) {
+
+        Intent S = new Intent(Intent.ACTION_SEND);
+        Uri SU = Uri.parse("android.resource://com.example.uocampus/"+ sr);
+        S.setType("String/text");
+        S.putExtra(Intent.EXTRA_STREAM, SU);
+        startActivity(Intent.createChooser(S, "Share post using"));
+    }
+
+//    public void onShareClick(String str) {
+//        Resources resources = getResources();
+//
+//        Intent emailIntent = new Intent();
+//        emailIntent.setAction(Intent.ACTION_SEND);
+//        // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
+//        emailIntent.putExtra(Intent.EXTRA_TEXT, str);
+//        emailIntent.setType("message/rfc822");
+//
+//        PackageManager pm = getPackageManager();
+//        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+//        sendIntent.setType("text/plain");
+//
+//
+//        Intent openInChooser = Intent.createChooser(emailIntent, str);
+//
+//        List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+//        List<LabeledIntent> intentList = new ArrayList<LabeledIntent>();
+//        for (int i = 0; i < resInfo.size(); i++) {
+//            // Extract the label, append it, and repackage it in a LabeledIntent
+//            ResolveInfo ri = resInfo.get(i);
+//            String packageName = ri.activityInfo.packageName;
+//            if(packageName.contains("android.email")) {
+//                emailIntent.setPackage(packageName);
+//            } else if(packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("mms") || packageName.contains("android.gm")) {
+//                Intent intent = new Intent();
+//                intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+//                intent.setAction(Intent.ACTION_SEND);
+//                intent.setType("text/plain");
+//                if(packageName.contains("twitter")) {
+//                    intent.putExtra(Intent.EXTRA_TEXT, str);
+//                } else if(packageName.contains("facebook")) {
+//                    // Warning: Facebook IGNORES our text. They say "These fields are intended for users to express themselves. Pre-filling these fields erodes the authenticity of the user voice."
+//                    // One workaround is to use the Facebook SDK to post, but that doesn't allow the user to choose how they want to share. We can also make a custom landing page, and the link
+//                    // will show the <meta content ="..."> text from that page with our link in Facebook.
+//                    intent.putExtra(Intent.EXTRA_TEXT, str);
+//                } else if(packageName.contains("mms")) {
+//                    intent.putExtra(Intent.EXTRA_TEXT,str);
+//                } else if(packageName.contains("android.gm")) {
+//                    intent.putExtra(Intent.EXTRA_TEXT, str);
+//                    intent.setType("message/rfc822");
+//                }
+//
+//                intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+//            }
+//        }
+//
+//        // convert intentList to array
+//        LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+//
+//        openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+//        startActivity(openInChooser);
+//    }
 }
